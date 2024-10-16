5542c51a3c3c1d29011856e7d960cd013cc750e4
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-16 15:52:33 +0300

    Add JUnit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void testHelloWorld() {
+        String expected = "Hello, World!";
+        String actual = "Hello, World!";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFail() {
+        fail();
+    }
+}
